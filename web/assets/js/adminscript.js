b077276facf6967fd6a9637a94cef3919413d669
Wolfgang Moritz <wolfmoritz@gmail.com>
2016-01-31 09:12:16 -0800

    Added summer note WYSIWYG editor.

--- web/assets/js/adminscript.js
+++ web/assets/js/adminscript.js
@@ -13,3 +13,38 @@ $('.deleteButton').on('click', function() {
   var reply = confirm('Are you sure you want to delete?');
   return reply;
 });
+
+
+// Text editor
+$('.wysiwyg-basic').summernote({
+  height: 500,
+  disableDragAndDrop: true,
+  toolbar: [
+    ['font', ['bold', 'italic', 'clear']],
+    ['para', ['ul', 'ol']],
+    ['insert', ['link', 'video', 'hr']],
+    ['view', ['fullscreen', 'codeview']],
+    ['help', ['help']]
+  ],
+  onpaste: function() {
+    var $editor = $(this);
+    // Pause to let new text actually paste
+    setTimeout(function () {
+      var text = $editor.code();
+      $editor.code('').html('<p>'+$(text).text()+'</p>');
+    }, 10);
+  }
+});
+
+$('.wysiwyg-full').summernote({
+  height: 500,
+  disableDragAndDrop: true,
+  onpaste: function() {
+    var $editor = $(this);
+    // Pause to let new text actually paste
+    setTimeout(function () {
+      var text = $editor.code();
+      $editor.code('').html('<p>'+$(text).text()+'</p>');
+    }, 10);
+  }
+});
